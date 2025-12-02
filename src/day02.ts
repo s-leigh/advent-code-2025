7f6ec32d40e35a18ea86419235ee1cbb01659319
Stephen Leigh <s-leigh@users.noreply.github.com>
2025-12-02 18:40:38 +0000

    Optimise

--- src/day02.ts
+++ src/day02.ts
@@ -1,56 +1,54 @@
-import { splitInputIntoLines } from "./common"
-
 export const day02Part01 = (input: string): number => {
-    const isValid = (id: string): boolean => {
-        if (id.length % 2 !== 0) return true
-        return id.slice(0, id.length / 2) !== id.slice(id.length / 2)
-    }
+  const isValid = (id: string): boolean => {
+    if (id.length % 2 !== 0) return true
+    return id.slice(0, id.length / 2) !== id.slice(id.length / 2)
+  }
 
-    const invalidIds = (start: number, end: number): number[] => {
-        const allIds = range(start, end)
-        return allIds.filter(id => !isValid(id.toString()))
-    }
+  const invalidIds = (start: number, end: number): number[] => {
+    const allIds = range(start, end)
+    return allIds.filter(id => !isValid(id.toString()))
+  }
 
-    const a = splitInputIntoLines(input)
-    const ranges = input.split(",")
-    const pairs = ranges.map(rangeStr => rangeStr.split("-").map(Number))
-    const invalid: number[] = pairs.map(([start, end]) => invalidIds(start, end)).flat()
-    return invalid.sum()
+  const ranges = input.split(",")
+  const pairs = ranges.map(rangeStr => rangeStr.split("-").map(Number))
+  const invalid: number[] = pairs.flatMap(([start, end]) => invalidIds(start, end))
+  return invalid.reduce((a, b) => a + b)
 }
 
 export const day02Part02 = (input: string): number => {
-    const uniqueParts = (str: string, parts: number): Set<string> => {
-        const partLength = str.length / parts
-        const result = new Set<string>()
-        for (let i = 0; i < parts; i++) {
-            result.add(str.substring(i * partLength, (i + 1) * partLength))
-        }
-        return result
+  const allPartsEqual = (str: string, parts: number): boolean => {
+    const partLength = str.length / parts
+    for (let i = 1; i < parts; i++) {
+      for (let j = 0; j < partLength; j++) {
+        if (str[j] !== str[i * partLength + j]) return false
+      }
     }
+    return true
+  }
 
-    const isValid = (id: string, splitDivisor: number = 2): boolean => {
-        if (splitDivisor > id.length) return true
-        if ((id.length / splitDivisor) % 1 !== 0) return isValid(id, splitDivisor + 1) // only divide by integers
-        const parts = uniqueParts(id, splitDivisor)
-        if (parts.size === 1) return false
-        return isValid(id, splitDivisor + 1)
+  const isValid = (id: string): boolean => {
+    for (let divisor = 2; divisor <= id.length; divisor++) {
+      if (id.length % divisor !== 0) continue // only integer divisors
+      if (allPartsEqual(id, divisor)) return false
     }
+    return true
+  }
 
-    const invalidIds = (start: number, end: number): number[] => {
-        const allIds = range(start, end)
-        return allIds.filter(id => !isValid(id.toString()))
-    }
+  const invalidIds = (start: number, end: number): number[] => {
+    const allIds = range(start, end)
+    return allIds.filter(id => !isValid(id.toString()))
+  }
 
-    const ranges = input.split(",")
-    const pairs = ranges.map(rangeStr => rangeStr.split("-").map(Number))
-    const invalid: number[] = pairs.map(([start, end]) => invalidIds(start, end)).flat()
-    return invalid.sum()
+  const ranges = input.split(",")
+  const pairs = ranges.map(rangeStr => rangeStr.split("-").map(Number))
+  const invalid: number[] = pairs.flatMap(([start, end]) => invalidIds(start, end))
+  return invalid.reduce((a, b) => a + b)
 }
 
 const range = (start: number, end: number): number[] => {
-    const result = []
-    for (let i = start; i <= end; i++) {
-        result.push(i)
-    }
-    return result
+  const result = []
+  for (let i = start; i <= end; i++) {
+    result.push(i)
+  }
+  return result
 }
